3e2b3f76e17defbcd5ddc3b8531cb0882970224a
Vincent Ho <vincentho711@gmail.com>
2024-01-05 17:29:13 +0000

    Add FreeRTOS blink example   * Add cmake files to enable compilation   * Edit the FreeRTOSConfig.h for using 2 cores   * Comments and formatting

--- freertos/blink/blink.cpp
+++ freertos/blink/blink.cpp
@@ -1,31 +1,40 @@
 /**
- * @file picow_hello_world_usb.cpp
+ * @file blink.cpp
  * @author Vincent Ho (mch79@cam.ac.uk)
- * @brief A test to send "hello world" to host via usb
+ * @brief 
  * @version 0.1
- * @date 2023-10-09
+ * @date 2024-1-5
  * 
  * @copyright Copyright (c) 2023
  * 
  */
 
+
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
+#include "FreeRTOS.h"
+#include "task.h"
 #include <stdio.h>
 #include <iostream>
 
-int main() {
-    stdio_init_all();
+static void led_task(void*)
+{   
     if (cyw43_arch_init()) {
         printf("Wi-Fi init failed");
-        return -1;
     }
     while (true) {
-        // printf("Hello, world!\n");
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         std::cout << "Hello, world! :):)" << std::endl;
-        sleep_ms(1000);
+        vTaskDelay(100);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(1000);
+        vTaskDelay(100);
     }
+}
+
+int main() {
+    stdio_init_all();
+
+    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
+    vTaskStartScheduler();
+    while (true) {};
 }
